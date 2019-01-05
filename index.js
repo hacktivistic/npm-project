6f9f021d1e1e6c9181928a86986e7afeb083b753
Robin Panta <hacktivistic@gmail.com>
2019-01-05 17:08:37 +0530

    [chore] add index file

---
+++ index.js
@@ -0,0 +1,16 @@
+/**
+ * npm-project
+ * Copyright(c) 2018
+ * MIT Licensed
+ *
+ * @author Robin Panta <hacktivistic@gmail.com>
+ */
+
+'use strict'
+
+/**
+ * Bootstrapping out Library
+ */
+var Example = require('./lib/Example')
+
+module.exports = Example
